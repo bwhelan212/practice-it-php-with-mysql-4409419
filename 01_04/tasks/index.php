00c27a253cbde94c94b980072508bfd1e4207882
Brodan Whelan <123424127+bwhelan212@users.noreply.github.com>
2024-04-28 22:54:39 +0000

    added sql connection, query, and listed results with php

--- 01_04/tasks/index.php
+++ 01_04/tasks/index.php
@@ -5,6 +5,27 @@
 // password: "mariadb"
 // database: "mariadb"
 // port:     3306
+// create database connectin
+$db = mysqli_connect("127.0.0.1", "mariadb", "mariadb", "mariadb", 3306);
+
+//test connection
+if (mysqli_connect_errno()) {
+  $msg = "Database connection unsucessful: ";
+  $msg .= mysqli_connect_error();
+  $msg .= " (" . mysqli_connect_errno() . ")";
+  exit($msg);
+}
+
+//database query
+$sql = "SELECT * FROM tasks ORDER BY priority";
+$result = mysqli_query($db, $sql);
+
+//check if query succeeded
+if (!$result) {
+  exit("Database query failed.");
+}
+
+//place returned data in array
 
 ?>
 
@@ -31,17 +52,24 @@
     	    <th>Description</th>
     	  </tr>
 
-        <?php // loop through tasks ?>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-      	    <td></td>
-      	  </tr>
-        <?php // end loop ?>
+        <?php // loop through tasks
+          while ($task = mysqli_fetch_assoc($result)) { ?>
+            <tr>
+              <td><?php echo $task['id'] ?></td>
+              <td><?php echo $task['priority'] ?></td>
+              <td><?php echo $task['completed'] == 1 ? 'true' : 'false'; ?></td>
+              <td><?php echo $task['description']; ?></td>
+            </tr>
+        <?php } // end loop ?>
     	</table>
 
     </section>
 
   </body>
 </html>
+
+<?php
+mysqli_free_result($result);
+
+mysqli_close($db);
+?>
